48197cccd7624eb5b4b18534f63f8a8bfc9f09c6
msmithfl <msmithfl@icloud.com>
2026-01-10 15:31:49 -0500

    update db migration

---
+++ backend/MovieVault.Api/Migrations/20260110203103_AppProductPosterPath.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieVault.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AppProductPosterPath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProductPosterPath",
+                table: "Movies",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductPosterPath",
+                table: "Movies");
+        }
+    }
+}
